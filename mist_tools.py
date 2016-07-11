00a6054374a8f05acd47d9d683c6a673d5b72663
ravisheth <sheth.u.ravi@gmail.com>
2016-07-11 15:09:54 -0700

    new parameter file, remove numpy dependency

--- mist_tools.py
+++ mist_tools.py
@@ -1,24 +1,18 @@
-#mist_tools v0.1
+#mist processing script
 #author: ravi sheth, wang lab, columbia university
-#last updated 4/5/2016
-
-import numpy as np
-import os
-from Bio import SeqIO
-from Bio.Seq import Seq
-from Bio.SeqRecord import SeqRecord
-from Bio.Alphabet import generic_dna
-from Bio.SeqIO import FastaIO
 import click
+import os
+from itertools import groupby
+import ConfigParser
 
-USEARCH = '/Users/ravi/Dropbox/wang_lab/mist_seq/mist_seq/usearch8.1.1861_i86osx32'
 FILE_DIR = os.path.dirname(__file__)
-
-bc1_7=np.loadtxt(FILE_DIR+'/bc1_7.txt',dtype=str)
-bc1_8=np.loadtxt(FILE_DIR+'/bc1_8.txt',dtype=str)
-bc1_9=np.loadtxt(FILE_DIR+'/bc1_9.txt',dtype=str)
-bc2_8=np.loadtxt(FILE_DIR+'/bc2.txt',dtype=str)
-anchor='GACCTGCG'
+config = ConfigParser.ConfigParser()
+config.read(FILE_DIR+'/param')
+anchor = config.get('default','anchor')
+bc1_7 = config.get('default','bc1_7').split(',')
+bc1_8 = config.get('default','bc1_8').split(',')
+bc1_9 = config.get('default','bc1_9').split(',')
+bc2_8= config.get('default','bc2_8').split(',')
 
 @click.group()
 def cli():
@@ -26,17 +20,19 @@ def cli():
 
 @cli.command()
 @click.argument('input', type=click.Path(exists=True))
+
 def process(input):
+	header=[]
 	seq=[]
-	fasta_id=[]
-	with click.progressbar(SeqIO.parse(open(input),'fasta'), label='reading sequences') as bar:
-		for record in bar:
-			seq.append(str(record.seq))
-			fasta_id.append(str(record.id).split('.')[0])
+	fiter=fasta_iter(input)
+	for rec in fiter:
+		h,s = rec
+		header.append(h.split('.')[0])
+		seq.append(s)
 
 	extract=[]
 	extract_anchor=[]
-	with click.progressbar(seq, label='mapping barcodes ') as bar:
+	with click.progressbar(seq, label='mapping barcodes') as bar:
 		for s in bar:
 			f_barcode, f_anchor = extract_barcodes(s)
 			extract.append(f_barcode)
@@ -44,49 +40,53 @@ def process(input):
 
 	extract_cnt=0
 	for i in extract:
-		if np.sum(i) > 0 and np.product(i) > 0:
+		if i[0] > 0 and i[1] > 0:
 			extract_cnt+=1
 
-	index_map=list(set(fasta_id))
+	index_map=list(set(header))
 	bc_map=[]
 	bc_count=[]
 	for n in index_map:
 		temp=[]
-		for i in np.arange(1,97):
-			for i2 in np.arange(1,97):
+		for i in range(1,97):
+			for i2 in range(1,97):
 				temp.append([i,i2])
 		bc_map.append(temp)
-		count=np.zeros(shape=9216)
+		count=[0]*9216
 		bc_count.append(count)
 
 	label = input.split('.')[0]
-	handle = open(label+'_out.fasta', "w")
-	fasta_out = FastaIO.FastaWriter(handle, wrap=None)
-	fasta_out.write_header()
-
+	fasta_out = open(label+'_out.fasta', "w")
 	write_cnt=0
-	with click.progressbar(range(len(extract)),label='writing output   ') as bar:
+	with click.progressbar(range(len(extract)),label='writing output  ') as bar:
 		for i in bar:
-			if np.product(extract[i]) != 0:
+			if extract[i][0] > 0 and extract[i][1] > 0:
 				s = filtered_seq(seq[i],extract_anchor[i])
 				if len(s) > 100 and len(s) < 400:
-					source_id=fasta_id[i]
+					source_id=header[i]
 					index_n=index_map.index(source_id)
 					bcn=bc_map[index_n].index(extract[i])
 					ind=bc_count[index_n][bcn]
-					fasta_out.write_record(SeqRecord(Seq(s,generic_dna), id=(source_id+str(bcn)+'.'+str(int(ind))),description=""))
+					fasta_out.write('>'+source_id+str(bcn)+'.'+str(int(ind))+'\n')
+					fasta_out.write(s+'\n')
 					bc_count[index_n][bcn]+=1
 					write_cnt+=1
-
-	fasta_out.write_footer()
+	fasta_out.close()
 	click.echo('read '+str(len(seq))+'; ',nl=False)
 	click.echo('mapped '+str(extract_cnt)+'; ',nl=False)
 	click.echo('wrote '+str(write_cnt)+' sequences')
 
+def fasta_iter(fasta_name):
+    fh = open(fasta_name)
+    faiter = (x[1] for x in groupby(fh, lambda line: line[0] == ">"))
+    for header in faiter:
+        header = header.next()[1:].strip()
+        seq = "".join(s.strip() for s in faiter.next())
+        yield header, seq
+
 def extract_barcodes(seq):
 	bc1_id=0
 	bc2_id=0
-
 	found_anchor=None
 	#attempt to make inexact match of anchor with single snp
 	bc_length=[7,8,9]
@@ -124,9 +124,9 @@ def hamming(a, b):
 	return dist
 
 def min_hamming(array, s):
-	if array.size == 1:
+	if len(array) == 1:
 		return hamming(array,s)
-	if array.size > 1:
+	if len(array) > 1:
 		minimum=len(s)
 		match=0
 		i=1
